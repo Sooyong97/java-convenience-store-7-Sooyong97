813268a2b4a302cbedbdaa27c30318219bae5b19
Sooyong97 <01071236398y@gmail.com>
2024-11-10 20:50:05 +0900

    feat: product.md 파일 읽어오는 함수 추가

--- src/main/java/store/Products.java
+++ src/main/java/store/Products.java
@@ -1,5 +1,8 @@
 package store;
 
+import java.io.*;
+import java.util.*;
+
 public class Products {
     private String name;
     private int price;
@@ -32,5 +35,32 @@ public class Products {
 
     public String getPromotion() {
         return promotion;
+    }
+
+    // md 파일 읽어오기
+    public static List<Products> read_md(String fileName) {
+        List<Products> productsList = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                // 첫 번째 줄 건너뛰기
+                if (line.startsWith("name")) {
+                    continue;
+                }
+
+                String[] data = line.split(",");
+                String name = data[0];
+                int price = Integer.parseInt(data[1]);
+                int quantity = Integer.parseInt(data[2]);
+                String promotion = data[3].equals("null") ? null : data[3];
+
+                // Products 객체를 만들어 리스트에 추가
+                Products product = new Products(name, price, quantity, promotion);
+                productsList.add(product);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return productsList;
     }
 }

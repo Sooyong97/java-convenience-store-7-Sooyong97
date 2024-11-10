4f3283967b4eb7b988bed3ace4ad6d79c626936f
Sooyong97 <01071236398y@gmail.com>
2024-11-10 23:48:26 +0900

    feat: View 기본 출력 추가

--- src/main/java/store/View.java
+++ src/main/java/store/View.java
@@ -4,6 +4,10 @@ import java.util.*;
 
 public class View {
 
+    public static void printHello() {
+        System.out.println("안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.");
+    }
+
     // 재고 현황 출력
     public static void displayProducts(List<Products> productsList) {
         for (Products product : productsList) {
@@ -22,4 +26,21 @@ public class View {
             System.out.println();
         }
     }
+
+    public static void printPurchase() {
+        System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
+    }
+
+    public static void printPromotions(String product, int count) {
+        System.out.println("현재 " + product + " " + count + "개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)");
+    }
+
+    public static void printMembership() {
+        System.out.println("멤버십 할인을 받으시겠습니까? (Y/N)");
+    }
+
+    public static void printReceipt() {
+        
+    }
+
 }

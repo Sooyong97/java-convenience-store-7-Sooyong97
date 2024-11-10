c1ed0f365466faded66ba7c7f814290ffb90bf94
Sooyong97 <01071236398y@gmail.com>
2024-11-11 01:33:23 +0900

    feat: View 함수 추가

--- src/main/java/store/View.java
+++ src/main/java/store/View.java
@@ -39,8 +39,36 @@ public class View {
         System.out.println("멤버십 할인을 받으시겠습니까? (Y/N)");
     }
 
-    public static void printReceipt() {
-        
+    public static void printReceipt(DiscountResult discountResult) {
+        printHeader();
+        printProducts(discountResult);
+        printFreeItems(discountResult);
+        printSummary(discountResult);
     }
 
+    private static void printHeader() {
+        System.out.println("==============W 편의점================");
+        System.out.println("상품명\t\t수량\t금액");
+    }
+
+    private static void printProducts(DiscountResult discountResult) {
+        for (Products product : discountResult.getFreeItems()) {
+            System.out.printf("%s\t\t%d \t%,d\n", product.getName(), product.getQuantity(), product.getPrice() * product.getQuantity());
+        }
+    }
+
+    private static void printFreeItems(DiscountResult discountResult) {
+        System.out.println("=============증 정===============");
+        for (Products freeItem : discountResult.getFreeItems()) {
+            System.out.printf("%s\t\t%d\n", freeItem.getName(), freeItem.getQuantity());
+        }
+    }
+
+    private static void printSummary(DiscountResult discountResult) {
+        System.out.println("====================================");
+        System.out.printf("총구매액\t\t\t%,d\n", discountResult.getTotalAmount());
+        System.out.printf("행사할인\t\t\t-%,d\n", discountResult.getPromotionDiscount());
+        System.out.printf("멤버십할인\t\t\t-%,d\n", discountResult.getMembershipDiscount());
+        System.out.printf("내실돈\t\t\t %,d\n", discountResult.getTotalAmount() - discountResult.getPromotionDiscount() - discountResult.getMembershipDiscount());
+    }
 }

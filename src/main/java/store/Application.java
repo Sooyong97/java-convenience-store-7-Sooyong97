dd952bf36b9d2501ad051bd4938088ca845e5272
Sooyong97 <01071236398y@gmail.com>
2024-11-11 01:34:48 +0900

    feat: 기능 실행 구현

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,15 @@
 package store;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<Promotion> promotions = Promotion.readPromotionsFromFile("src/main/resources/promotions.md");
+        List<Products> products = Products.read_md("src/main/resources/products.md");
+
+        StoreController storeController = new StoreController(products, promotions);
+
+        storeController.run();
     }
 }
